6f7689ec0bce05758751400a088308d107feb729
Drew Schillinger <drew@doctorew.com>
2023-12-08 16:40:35 -0500

    calling pocketmorties data returns data!!!

--- apps/graphql/src/graphql/rickmorty/resolvers.ts
+++ apps/graphql/src/graphql/rickmorty/resolvers.ts
@@ -1,111 +1,54 @@
 import axios from 'axios';
 
-// Assuming Character represents the structure of a character from the Rick and Morty API
+// Interfaces for to provide typesafety for TypeScript
 interface Character {
     id: string;
     name: string;
+    status: string;
+    species: string;
+    image: string;
     // ... other fields that characters contain
 }
 
+interface PocketMorty {
+    id: number;
+    name: string;
+    type: string;
+    image: string;
+    evolution: number[];
+    evolutions: number[];
+    rarity: number;
+    hp: number;
+    atk: number;
+    def: number;
+    spd: number;
+    // ... other fields as necessary
+}
 
-// Helper function to determine if a Rick and Morty should be associated
-const shouldAssociate = (rick: any, morty: any) => {
-    if (rick.origin?.id && morty.origin?.id && rick.origin.id === morty.origin.id) {
-        return true;
+// Utility function to fetch data from a URL
+const fetchData = async (url: string) => {
+    try {
+        const response = await axios.get(url);
+        return response.data.results || response.data;  // Accommodate different response structures
+    } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error);
+        return [];
     }
-    if (rick.origin?.name === 'unknown' && morty.origin?.name === 'unknown') {
-        const rickNamePattern = rick.name.replace('Rick', '').trim();
-        const mortyNamePattern = morty.name.replace('Morty', '').trim();
-        return rickNamePattern === mortyNamePattern;
-    }
-    if (rick.location?.id && morty.location?.id && rick.location.id === morty.location.id && rick.location.id !== '3') {
-        return true;
-    }
-    return false;
 };
 
-// Function to transform Pocket Mortys data into a format similar to Rick and Morty API data
-const transformPocketMortyData = (pocketMortyData: any) => {
-    // Implement logic to transform and return Pocket Morty data
-    // Map fields from Pocket Morty to match those in the Character type
-    return {
-        id: pocketMortyData.assetid,
-        name: pocketMortyData.name,
-        // Add other necessary fields or placeholders
-    };
-};
-
-
-const rickMortyResolvers = {
+// Resolver functions
+const resolvers = {
     Query: {
-        charactersByName: async (_: any, args: { name: string }) => {
-            const query = `
-                {
-                    characters(filter: {name: "${args.name}"}) {
-                        results {
-                            id
-                            name
-                            status
-                            species
-                            type
-                            gender
-                            image
-                            origin {
-                                id
-                                name
-                            }
-                            location {
-                                id
-                                name
-                            }
-                        }
-                    }
-                }`;
-            try {
-                const response = await axios.post('https://rickandmortyapi.com/graphql', {query});
-                return response.data.data.characters.results;
-            } catch (error) {
-                console.error("Error fetching characters:", error);
-                return [];
-            }
+        pocketMorties: async (): Promise<PocketMorty[]> => {
+            return fetchData('https://www.doctorew.com/shuttlebay/pocketmorties.json');
         },
-
-        rickAndMortyAssociations: async () => {
-            try {
-                // Fetching data from Rick and Morty API
-                const ricksResponse = await axios.get('https://rickandmortyapi.com/api/character/?name=Rick');
-                const mortiesResponse = await axios.get('https://rickandmortyapi.com/api/character/?name=Morty');
-                const ricks = ricksResponse.data.results;
-                const morties = mortiesResponse.data.results;
-
-                // Fetching data from Pocket Mortys API
-                const pocketMortysResponse = await axios.get('https://pocketmortys.net/components/com_pocketmortys/json/datatables/MortysTable_en.json');
-                const pocketMortys = pocketMortysResponse.data.data.map(transformPocketMortyData);
-
-                // Combine and process data from both sources
-                const combinedMorties = [...morties, ...pocketMortys];
-                let orphanedMorties = [...combinedMorties];
-
-                const associations = ricks.map((rick:Character) => {
-                    const associatedMorties = combinedMorties.filter(morty => shouldAssociate(rick, morty));
-                    orphanedMorties = orphanedMorties.filter(morty => !associatedMorties.includes(morty));
-                    return { rick, morties: associatedMorties };
-                });
-
-
-                return {
-                    associations,
-                    orphanedMorties
-                };
-            } catch (error) {
-                console.error("Error in rickAndMortyAssociations:", error);
-                return {
-                    associations: [],
-                    orphanedMorties: []
-                };
-            }
+        ricks: async (): Promise<Character[]> => {
+            return fetchData('https://rickandmortyapi.com/api/character/?name=Rick');
         },
-    }
+        morties: async (): Promise<Character[]> => {
+            return fetchData('https://rickandmortyapi.com/api/character/?name=Morty');
+        },
+    },
 };
 
-export default rickMortyResolvers;
+export default resolvers;
--- apps/graphql/src/server.ts
+++ apps/graphql/src/server.ts
@@ -1,13 +1,13 @@
 //src/server.ts
 
-import express, { Express, Request, Response, NextFunction } from 'express';
+import express, {Express, Request, Response, NextFunction} from 'express';
 
-import { ApolloServer } from 'apollo-server-express';
-import { readFileSync } from 'fs';
+import {ApolloServer} from 'apollo-server-express';
+import {readFileSync} from 'fs';
 import path from 'path';
 import rickMortyResolvers from './graphql/rickmorty/resolvers';
 import travelDataResolvers from './graphql/traveldata/resolvers';
-import { RedisCache } from 'apollo-server-cache-redis';
+import {RedisCache} from 'apollo-server-cache-redis';
 
 const app: express.Application = express();
 const PORT = 4000;
@@ -16,56 +16,51 @@ const PORT = 4000;
 const rickMortyTypeDefs = readFileSync(path.join(__dirname, 'graphql/rickmorty/schema.graphql'), 'utf-8');
 const travelDataTypeDefs = readFileSync(path.join(__dirname, 'graphql/traveldata/schema.graphql'), 'utf-8');
 
+/*
 const redisCache = new RedisCache({
     host: 'redis',
     port: 6379
 });
+*/
 
 // Create ApolloServer instances for both endpoints
 const rickMortyServer = new ApolloServer({
     typeDefs: rickMortyTypeDefs,
     resolvers: rickMortyResolvers,
-    cache: redisCache,
-    context: ({ req, res }) => ({
+    //cache: redisCache,
+    context: ({req, res}) => ({
         req,
         res,
-        cache: redisCache,
+        //  cache: redisCache,
     })
 });
 
 const travelDataServer = new ApolloServer({
     typeDefs: travelDataTypeDefs,
     resolvers: travelDataResolvers,
-    cache: redisCache,
-    context: ({ req, res }) => ({
+    //   cache: redisCache,
+    context: ({req, res}) => ({
         req,
         res,
-        cache: redisCache,
+        //cache: redisCache,
     })
 });
 
 // Health Check Endpoint
 app.get('/health', async (req: Request, res: Response) => {
-    try {
-        // Optional: Check Redis connection
-        await redisCache.get('health-check');
-
-        // Send a positive response if everything is fine
-        res.status(200).send('OK');
-    } catch (error) {
-        // If Redis is down or any error occurs, send a failure response
-        res.status(500).send('Error');
-    }
+    // Update this endpoint to check the health of other services if necessary
+    res.status(200).send('OK');
 });
+
 
 
 // Create an asynchronous function to start the servers and apply middleware
 async function startServer() {
     await rickMortyServer.start();
-    rickMortyServer.applyMiddleware({ app: app as any, path: '/rickmorty' });
+    rickMortyServer.applyMiddleware({app: app as any, path: '/rickmorty'});
 
     await travelDataServer.start();
-    travelDataServer.applyMiddleware({ app: app as any, path: '/traveldata' });
+    travelDataServer.applyMiddleware({app: app as any, path: '/traveldata'});
 
     app.listen(PORT, () => {
         console.log(`Rick and Morty GraphQL API available at http://localhost:${PORT}${rickMortyServer.graphqlPath}`);
