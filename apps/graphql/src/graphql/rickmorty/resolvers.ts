67b3a00cb8b537a55e293321c5a10a8e651700da
Drew Schillinger <drew@doctorew.com>
2023-12-08 20:57:57 -0500

    filtering and sorting working

--- apps/graphql/src/graphql/rickmorty/resolvers.ts
+++ apps/graphql/src/graphql/rickmorty/resolvers.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Interfaces for to provide typesafety for TypeScript
+// Interfaces for TypeScript type safety
 interface Character {
     id: string;
     name: string;
@@ -17,31 +17,70 @@ interface PocketMorty {
     image: string;
     evolution: number[];
     evolutions: number[];
-    rarity: number;
-    hp: number;
-    atk: number;
-    def: number;
-    spd: number;
-    // ... other fields as necessary
+    rarity: string;
+    basehp: number;
+    baseatk: number;
+    basedef: number;
+    basespd: number;
+    basexp: number;
+    stattotal: number;
+    dimensions: string;
+    where_found: [string];
 }
+
 
 // Utility function to fetch data from a URL
 const fetchData = async (url: string) => {
     try {
         const response = await axios.get(url);
-        return response.data.results || response.data;  // Accommodate different response structures
+        return response.data.results || response.data; // Accommodate different response structures
     } catch (error) {
         console.error(`Error fetching data from ${url}:`, error);
         return [];
     }
 };
 
-// Resolver functions
+// Filtering utility for PocketMorties
+const filterPocketMorties = (data: any[], args: any) => {
+    return data.filter(morty => {
+        return (!args.type || args.type.includes(morty.type)) &&
+            (!args.rarity || args.rarity.includes(morty.rarity)) &&
+            (args.basexp === undefined || morty.basexp === args.basexp) &&
+            (args.basehp === undefined || morty.basehp === args.basehp) &&
+            (args.baseatk === undefined || morty.baseatk === args.baseatk) &&
+            (args.basedef === undefined || morty.basedef === args.basedef) &&
+            (args.basespd === undefined || morty.basespd === args.basespd) &&
+            (args.stattotal === undefined || morty.stattotal === args.stattotal) &&
+            (!args.dimensions || args.dimensions.includes(morty.dimensions)) &&
+            (!args.where_found || args.where_found.some((location: any) => morty.where_found.includes(location)));
+    });
+};
+
+
+// Utility function for sorting and limiting
+const sortAndLimit = (data: any, sortBy: string, limit: number) => {
+    let sortedData = data;
+    if (sortBy) {
+        sortedData = [...data].sort((a, b) => b[sortBy] - a[sortBy]);
+    }
+    if (typeof limit === 'number' && limit >= 0) {
+        sortedData = sortedData.slice(0, limit);
+    }
+    return sortedData;
+};
+
+// Update the resolvers
 const resolvers = {
     Query: {
-        pocketMorties: async (): Promise<PocketMorty[]> => {
-            return fetchData('https://www.doctorew.com/shuttlebay/pocketmorties.json');
+        pocketMorties: async (_: any, args: any) => {
+            const data = await fetchData('https://www.doctorew.com/shuttlebay/cleaned_pocket_morties.json');
+            let filteredData = filterPocketMorties(data, args);
+
+            // Use the provided sortBy and limit arguments
+            return sortAndLimit(filteredData, args.sortBy, args.limit);
         },
+
+
         ricks: async (): Promise<Character[]> => {
             return fetchData('https://rickandmortyapi.com/api/character/?name=Rick');
         },
