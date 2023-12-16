36e5c137f62e9703de3915a5355eb8e66b60ffc2
Drew Schillinger <drew@doctorew.com>
2023-12-15 22:14:15 -0500

    ampligying -- tryinng to get to build

--- apps/web/app/morty/[id]/page.tsx
+++ apps/web/app/morty/[id]/page.tsx
@@ -1,28 +1,39 @@
-// apps/web/app/morty/[id]/page.tsx
-
 'use client';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_POCKET_MORTY_QUERY } from '../../../lib/graphqlQueries'; // Ensure this is defined and imported correctly
+import { GET_POCKET_MORTY_QUERY } from '../../../lib/graphqlQueries';
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+// Define the Morty type
+interface Morty {
+    name: string;
+    id: number;
+    type: string;
+    assetid: string;
+    // Add other properties as needed
+}
 
 // Initialize Apollo Client
 const apolloClient = new ApolloClient({
-    link: new HttpLink({ uri: 'https://mms-graph.doctorew.com/rickmorty' }), // Use the correct endpoint
+    link: new HttpLink({ uri: 'https://mms-graph.doctorew.com/rickmorty' }),
     cache: new InMemoryCache(),
 });
 
 const MortyPage = () => {
     const pathname = usePathname();
     const id = parseInt(pathname.split('/').pop() as string, 10);
-    const [morty, setMorty] = useState(null);
+    const [morty, setMorty] = useState<Morty | null>(null);
 
     const { loading, error } = useQuery(GET_POCKET_MORTY_QUERY, {
         variables: { id },
         client: apolloClient,
         skip: isNaN(id),
-        onCompleted: (data) => setMorty(data.pocketMorty),
+        onCompleted: (data) => {
+            if (data && data.pocketMorty) {
+                setMorty(data.pocketMorty);
+            }
+        },
     });
 
     useEffect(() => {
@@ -45,9 +56,9 @@ const MortyPage = () => {
 
             <div className="images-and-info">
                 <img src={`https://pocketmortys.net/media/com_pocketmortys/assets/${morty.assetid}Front.png`}
-                     alt={`${morty.name} Front`}/>
+                     alt={`${morty.name} Front`} />
                 <img src={`https://pocketmortys.net/media/com_pocketmortys/assets/${morty.assetid}Back.png`}
-                     alt={`${morty.name} Back`}/>
+                     alt={`${morty.name} Back`} />
                 {/* Info grid */}
             </div>
         </div>
